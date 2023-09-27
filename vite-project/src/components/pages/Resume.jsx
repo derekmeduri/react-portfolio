f3e971a350c330bb87d8f22ca7f12114a57dffde
Derek Meduri <derekmeduri@gmail.com>
2023-09-27 09:46:56 -0400

    adding fav icon

--- vite-project/src/components/pages/Resume.jsx
+++ vite-project/src/components/pages/Resume.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import resume from '';
+import resume from '../../assets/dmeduriresume1.png';
+import resume2 from '../../assets/dmeduriresume2.png';
 
 const Resume = () => (
     <div className='container'>
         <h1>Resume</h1>
         <br></br>
         <p>
-            <img>
+            <img src={resume} id='resume' alt='Derek Meduri Wrok Experience'>
+            </img>
+            <img src={resume2} id='resume' alt='Derek Meduri Sample Applications'>
             </img>
         </p>
         <br></br>
         <p>
-            <a href=''>
-                Download Resume
+            <a href='https://github.com/derekmeduri/react-portfolio/blob/main/vite-project/src/assets/DerekMeduriResume.pdf' target='_blank'>
+                Download a PDF of my Resume
             </a>
         </p>
     </div>
