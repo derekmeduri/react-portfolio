06d98df9667f49284a8f2292d04d8556e5a941f0
Derek Meduri <derekmeduri@gmail.com>
2023-09-27 12:23:57 -0400

    adding styling to about and contact

--- vite-project/src/components/pages/Contact.jsx
+++ vite-project/src/components/pages/Contact.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import email from '../../assets/email.png';
+import phone from '../../assets/phone.png';
+import lines from '../../assets/lines.png';
+
+const styles = {
+    jumbotron: {
+        backgroundColor: "white",
+        marginTop: 100,
+        marginBottom: 100
+    },
+    icon: {
+        height: 20,
+        marginRight:5
+    },
+    lines: {
+        height: 20,
+        paddingLeft: 10,
+        paddingRight: 10
+    }
+}
 
 const Contact = () => (
-    <div className='container'>
-        <h1>Let's Work Toegether!</h1>
+    <div className='container-sm container-md container-lg container-xl'>
+        <div className='jumbotron shadow' style={styles.jumbotron}>
+        <h1 className='display-4 text-center'>Let's Work Toegether!</h1>
         <br></br>
-        <p>Want to connect? Reach me below:</p>
-        <p><a href='mailto:derekmeduri@gmail.com' rel='noreferrer' target='_blank'>derekmeduri@gmail.com</a>
+        <p className='text-center'>Want to connect? Reach me below:</p>
+
+        <p className='text-center'>
+            <img src={email}
+            style={styles.icon}></img>
+            <a href='mailto:derekmeduri@gmail.com' rel='noreferrer' target='_blank'>derekmeduri@gmail.com</a>
+
+            <img src={lines}
+            style={styles.lines}></img>
+
+            <img src={phone}
+            style={styles.icon}></img>
         330-283-8221
         </p>
-        
+        </div>
     </div>
 );
 
