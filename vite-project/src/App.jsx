576ae0cba498aae86fd480f4bde12a1ffb22ec21
Derek Meduri <derekmeduri@gmail.com>
2023-09-27 11:26:42 -0400

    adding images and building card body

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,29 @@
-import './App.css'
+import React from "react";
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Portfolio from './components/pages/Portfolio';
 import Resume from './components/pages/Resume';
 import Footer from './components/Footer';
-import { HashRouter, Route } from 'react-router-dom';
+
+
 
 const App = () => 
 <div>
   <HashRouter>
   <Nav/>
     <div>
-    <switch>
+    <Switch>
       <Route exact path={'/'} Component={About} />
       <Route exact path={'/portfolio'} Component={Portfolio}/>
       <Route exact path={'/resume'} Component={Resume}/>
       <Route exact path={'/contact'} Component={Contact}/>
-     </switch>
+     </Switch>
      </div>
      <Footer/>
      </HashRouter>
      
 </div>
 
-export default App
+export default App;
--- vite-project/src/components/ProjectCard.jsx
+++ vite-project/src/components/ProjectCard.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
+const styles = {
+    card: {
+        marginBottom: 30
+    },
+
+    button: {
+        marginRight: 5
+    }
+}
+
 function ProjectCard(props) {
     return (
-        <div className=''>
-            <div>
+        <div className='col-sm-6'>
+            <div className='card tex-center' style={styles.card}>
                 <img src={props.image} alt={props.title}>
                 </img>
-                <div>
-                    <h5>{props.title}</h5>
-                    <a href={props.deployed}>View Project</a>
-                    <a href={props.repo}>GitHub Repo</a>
+                <div className='card-body'>
+                    <h5 className='card-title'>{props.title}</h5>
+                    <a href={props.deployed} target='_blank' >View Project</a>
+                    <a href={props.repo} target='_blank'>GitHub Repo</a>
                 </div>
             </div>
         </div>
