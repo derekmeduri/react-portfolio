bd8a64182764865f081eb2efabac67e4fa253749
Derek Meduri <derekmeduri@gmail.com>
2023-10-23 10:21:19 -0400

    redoing about me

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -28,8 +28,8 @@ const About = () => (
                 src={me}
                 alt='Derek Meduri'></img>
                 <p>I'm Derek!</p>
-                <p>I am a freelance product developer. Currently enrolled in The Ohio State University Full Stack Web Development Coding Bootcamp. My degree in Fashion Merchandising lead me to New York City, but growing up in Ohio brought me back to Columbus.</p>
-                <p>My professional career started in retail, selling shoes, in 2007. Upon graduation from Kent State University in 2015 I began working in corporate fashion as a Product Developer. I have worked closely with fashion designers to bring their visions to life. Now, I have shifted my focus to software development where I have been learning to code for the past year or so. I look forward to bringing many more visions to life with the knowledge and skills I have obtained.</p>
+                <p>A freelance product developer and full-stack engineer. As a recent graduate of The Ohio State University Full-Stack Web Development certificate program I am looking for full-time or part-time work in web development. My degree in Fashion Merchandising lead me to New York City, but growing up in Ohio brought me back to Columbus.</p>
+                <p>My professional career started in retail, selling shoes, in 2007. Upon graduation from Kent State University in 2015 I began working in corporate fashion as a Product Developer. I have worked closely with fashion designers to bring their visions to life. Now, I have shifted my focus to software development where I have been learning to code for the past year. I look forward to bringing many more visions to life with the knowledge and skills I have obtained.</p>
                 <p>I am a creative, passionate and dedicated Web Developer with strong leadership, communication and organizational skills. If you would like to see some of my recent applications checkout out my portfolio and resume tabs in the navigation.</p>
                 <p>Thanks for stopping by!</p>
             </div>
